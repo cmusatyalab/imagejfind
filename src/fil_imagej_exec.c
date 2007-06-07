fd091b8df5e7b275d90dd9e4050f7b810ae36e9c
Jason Ganetsky <jganetsk@cs.cmu.edu>
2007-06-06 22:24:21 -0400

    Fixing this branch nightmare.. reverting to non-VMWare fil_imagej_exec.c
    Due to my misuse of CVS, I created the "branch nightmare"
    I had important fixes for fil_imagej_exec.c for the VMWare branch,
    but submitted those fixes to the master branch accidentally!
    This caused the master branch to have code meant for the VMWare branch
    
    This commit reverts fil_imagej_exec.c to the non-VMWare version
    originally from this branch

--- src/fil_imagej_exec.c
+++ src/fil_imagej_exec.c
@@ -22,34 +22,34 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/socket.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include <unistd.h>
-#include <netdb.h>
-
-#define PORT 6822
 
 #ifndef IMAGEJ_EXE_PATH
 #error Must specify IMAGEJ_EXE_PATH as compilation flag
 #endif
 
 struct filter_instance {
-   int ij_fd;
+   int ij_pid;
+   int ij_to_fd;
+   int ij_from_fd;
+   FILE *ij_to_file;
+   FILE *ij_from_file;
    int macro_len;
    char *macro;
 };
 
 static void transmit_image(int img_width, int img_height,
-                           const unsigned char *diamond_buf, int fd)
+                           const unsigned char *diamond_buf, FILE *fp)
 {
    int net_img_height = htonl(img_height);
    int net_img_width = htonl(img_width);
 
    printf("Sending %d x %d image...\n", img_height, img_width);
    
-   send(fd, &net_img_width, sizeof(int), MSG_MORE);
-   send(fd, &net_img_height, sizeof(int), MSG_MORE);
+   fwrite(&net_img_width, sizeof(net_img_width), 1, fp);
+   fwrite(&net_img_height, sizeof(net_img_height), 1, fp);
    
    int row, col;
    const unsigned char * diamond_ptr = diamond_buf;
@@ -57,42 +57,26 @@ static void transmit_image(int img_width, int img_height,
    
    for (row = 0; row < img_height; row++) {
       for (col = 0; col < img_width; col++) {
-         send(fd, &zero, 1, MSG_MORE);
-         int flags;
-         if ((row == img_height - 1) && (col == img_width - 1)) 
-            flags = 0;
-         else
-            flags = MSG_MORE;
-         send(fd, diamond_ptr, 3, flags);
+         fwrite(&zero, 1, 1, fp);
+         fwrite(diamond_ptr, 3, 1, fp);
          diamond_ptr += 4;
       }
    }
 }
 
-static void transmit_macro(int macro_len, char *macro, int fd)
+static void transmit_macro(int macro_len, char *macro, FILE *fp)
 {
    int net_macro_len = htonl(macro_len);
 
    printf("Sending macro...\n");
-   send(fd, &net_macro_len, sizeof(int), MSG_MORE);
-   send(fd, macro, macro_len, 0);
+   fwrite(&net_macro_len, sizeof(net_macro_len), 1,  fp);
+   fwrite(macro, macro_len, 1, fp);
 }
 
-static double get_result(int fd)
+static double get_result(FILE *fp, int fd)
 {
    double result;
-   char buf[512];
-   memset(buf, 0, sizeof(buf));
-   int nread = 0, read_result;
-   do {
-       read_result = recv(fd, buf + nread, 1, 0);
-       if (read_result < 0) {
-	 perror("recv");
-         return 0.0;
-       } else
-         nread += read_result;
-   } while (buf[nread - 1] != '\n');
-   sscanf(buf, "%lE", &result);
+   fscanf(fp, "%lE", &result);
    return result;
 }
 
@@ -100,47 +84,55 @@ int f_init_imagej_exec (int num_arg, char **args, int bloblen,
                         void *blob_data, const char *filter_name,
                         void **filter_args)
 {
-   int ij_socket = socket(AF_INET, SOCK_STREAM, 0);
-   
-   struct hostent host, *result;
-   char buf[1024];
-   int h_errno;
-   if ((gethostbyname2_r("ijvm", AF_INET, &host, buf, sizeof(buf), &result, &h_errno) < 0) || (!result)) {
-      perror("gethost");
-      *filter_args = NULL;
-   } else {
-       struct in_addr *sin_addr = (struct in_addr *)(result->h_addr_list[0]);
-       struct sockaddr_in sock_addr;
-       memset(&sock_addr, 0, sizeof(struct sockaddr_in));
-       sock_addr.sin_family = AF_INET;
-       sock_addr.sin_port = htons(PORT);
-       memcpy(&sock_addr.sin_addr, sin_addr, sizeof(sock_addr.sin_addr));
-       if (connect(ij_socket, (struct sockaddr *)&sock_addr, sizeof(struct sockaddr_in)) < 0) {
-         perror("connect");
-         *filter_args = NULL;
-       } else {
-         struct filter_instance *inst = 
-           (struct filter_instance *)malloc(sizeof(struct filter_instance));
-   
-         inst->ij_fd = ij_socket;
-         inst->macro_len = bloblen;
-         inst->macro = malloc(bloblen);
-         if (bloblen > 0) {
-            memcpy(inst->macro, blob_data, bloblen);
-         }
-   
-        *filter_args = inst;
-       } 
-   }
+   int child_in_fd[2], child_out_fd[2];
 
+   pipe(child_in_fd);
+   pipe(child_out_fd);
+
+   int child_pid = fork();
+
+   if (child_pid == 0) {
+      close(child_in_fd[1]);
+      close(child_out_fd[0]);
+      dup2(child_in_fd[0], STDIN_FILENO);
+      dup2(child_out_fd[1], STDOUT_FILENO);
+      
+      if (chdir(IMAGEJ_EXE_PATH) < 0) {
+         perror("Could not enter ImageJ directory");
+         exit(0);
+      }
+      setenv("DISPLAY", "localhost:100", 1);
+
+      execlp("java", "java", "-server", "-cp", "ij.jar:.", 
+             "ijloader.IJLoader", NULL);
+   } else {
+      close(child_in_fd[0]);
+      close(child_out_fd[1]);
+ 
+      struct filter_instance *inst = 
+         (struct filter_instance *)malloc(sizeof(struct filter_instance));
+   
+      inst->ij_pid = child_pid;
+      inst->ij_to_fd = child_in_fd[1];
+      inst->ij_from_fd = child_out_fd[0];
+      inst->ij_to_file = fdopen(inst->ij_to_fd, "w");
+      inst->ij_from_file = fdopen(inst->ij_from_fd, "r");
+      inst->macro_len = bloblen;
+      inst->macro = malloc(bloblen);
+      if (bloblen > 0) {
+         memcpy(inst->macro, blob_data, bloblen);
+      }
+   
+      *filter_args = inst;
+
+   }    
+   
    return 0;
 }
 
 int f_eval_imagej_exec (lf_obj_handle_t ohandle, void *filter_args)
 {
    struct filter_instance *inst = (struct filter_instance *)filter_args;
-   if (!inst)
-     return 0;
 
    size_t len;
    unsigned char *diamond_attr;
@@ -157,14 +149,13 @@ int f_eval_imagej_exec (lf_obj_handle_t ohandle, void *filter_args)
 
    lf_ref_attr(ohandle, "_rgb_image.rgbimage", &len, &diamond_attr);
 
-   transmit_image(width, height, diamond_attr, inst->ij_fd);
-   transmit_macro(inst->macro_len, inst->macro, inst->ij_fd);
+   transmit_image(width, height, diamond_attr, inst->ij_to_file);
+   transmit_macro(inst->macro_len, inst->macro, inst->ij_to_file);
+   fflush(inst->ij_to_file);
    printf("New image + macro sent...\n");
    fflush(stdout);
    
-   double result = get_result(inst->ij_fd);
-   printf("Result: %lf\n", result);
-   fflush(stdout);
+   double result = get_result(inst->ij_from_file, inst->ij_from_fd);
    
    lf_write_attr(ohandle, "_matlab_ans.double", sizeof(double), (unsigned char *)&result);
 
@@ -174,10 +165,15 @@ int f_eval_imagej_exec (lf_obj_handle_t ohandle, void *filter_args)
 int f_fini_imagej_exec (void *filter_args)
 {
    struct filter_instance *inst = (struct filter_instance *)filter_args;
-   if (!inst)
-     return 0;
 
-   close(inst->ij_fd);
+   int status;
+   kill(inst->ij_pid, SIGKILL);
+   waitpid(inst->ij_pid, &status, 0);
+
+   fclose(inst->ij_to_file);
+   fclose(inst->ij_from_file);
+   close(inst->ij_to_fd);
+   close(inst->ij_from_fd);
    
    if (inst->macro) {
       free(inst->macro);
